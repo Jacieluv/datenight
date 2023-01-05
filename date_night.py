b43fa49a8f2e997aa98812c064d21065f9fa419d
Jacieluv <cjaciedd@yahoo.com>
2023-01-05 10:12:15 -0800

    Add columns and restaurants to try

--- date_night.py
+++ date_night.py
@@ -19,19 +19,31 @@ from st_aggrid import GridOptionsBuilder, AgGrid, GridUpdateMode, DataReturnMode
 # create lists
 staying_in = ['Make Dinner Together', 'Get Takeout or Delivery', 'Valorant', 'Play Board Games', 'Play Card Games', 'Do a Puzzle', 'Paint', 'Update a Travel List', 'Update a Bucket List', 'Create a Photo Book']
 going_out = ['Eat Out', 'Watch a movie', 'Picnic', 'Somewhere Tiktok famous', 'Instagram Aesthetic Photos', 'Play Laser Tag', 'Visit a Museum', 'Mall', 'Bowling', 'Comedy Show', 'Roller Skating', 'Ice Skating', 'See a Psychic', 'Be a Tourist', 'Amusement Park', 'Take a cooking or baking class', 'Go on a hike', 'Visit an animal shelter', 'Recreate your first date', 'Go on an alphabet date', 'Volunteer', 'Run or walk a charity 5k', 'Archery', 'Axe Throwing', 'Musical']
+restaurant_to_try = ['Daiki Sushi', 'Gao Viet Kitchen', 'Avenida', 'Hummus Mediterranean Kitchen', 'Foreigner', 'Tomatina']
 
 # title
 st.markdown("<h1 style = 'text-align: center;'>Date Night</h1>", unsafe_allow_html=True)
 
-# Create sidebar options for axes and grouping 
-st.markdown("<h2 style = 'text-align: left;'>What do you want to do?</h2>", unsafe_allow_html=True)
-stay_in = st.button("Stay In")
-if stay_in:
-    st.write(random.choice(staying_in))
+# add columns
+col1, col2 = st.columns(2)
+with col1:
 
-go_out = st.button("Go Out")
-if go_out:
-    st.write(random.choice(going_out))
+    # ask what to do buttons 
+    st.markdown("<h2 style = 'text-align: left;'>Want to...?</h2>", unsafe_allow_html=True)
+    stay_in = st.button("Stay In")
+    if stay_in:
+        st.write(random.choice(staying_in))
+
+    go_out = st.button("Go Out")
+    if go_out:
+        st.write(random.choice(going_out))
+
+with col2:
+    st.markdown("<h2 style = 'text-align: left;'>Going Out</h2>", unsafe_allow_html=True)
+    st.markdown("<h3 style = 'text-align: left;'>Restuarants to Try</h3>", unsafe_allow_html=True)
+    random_restaurant = st.button("Random Restaurant")
+    if random_restaurant:
+        st.write(random.choice(restaurant_to_try))
 
 # cute pic of us
 image = Image.open('ok_buddy.jpg')
